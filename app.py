2d3e1c07978942f8fc9e65ece257e74e2593601a
psukez <parroyoz@me.com>
2021-08-27 00:42:42 -0400

    Add Heroku deployment files

--- app.py
+++ app.py
@@ -1,68 +1,76 @@
-from flask import Flask, render_template, make_response,request,redirect,url_for, session
-from turbo_flask import Turbo
 import random
 import threading
 import time
 
+from flask import Flask, render_template, request
+from turbo_flask import Turbo
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = '123456'
 turbo = Turbo(app)
-PNI = "ON"
-global numeroS
-numeroS = 0
-global numeroD
-numeroD = 0
-global Fcmin
+
+numeroS = 1
+numeroD = 1
 Fcmin = 60
-global Fcmax
 Fcmax = 100
-global Satmin
 Satmin = 90
-global Satmax
 Satmax = 100
+PNI = 0
+
 
 def update_load():
     with app.app_context():
+        global PNI
+        print(PNI)
         while True:
             time.sleep(2)
-            turbo.push(turbo.replace(render_template('pni.html'), 'loadB'))
             turbo.push(turbo.replace(render_template('loadavg.html'), 'load'))
+            #print(PNI)
+            #print(numeroS)
+            #print(numeroD)
+            if PNI == 1:
+                PNI = 0
+                turbo.push(turbo.replace(render_template('pni.html'), 'loadB'))
 
-#def pni():
-  #  global numeroS
-   # global numeroD
-    #if numeroS and numeroD != 0:
-     #   if pressed button:
-      #      display PNI
 
 @app.before_first_request
 def before_first_request():
     threading.Thread(target=update_load).start()
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+
 @app.context_processor
 def inject_load():
     global numeroS
+    nS = int(numeroS)
     global numeroD
+    nD = int(numeroD)
     global Fcmin
     global Fcmax
     global Satmin
     global Satmax
+    global PNI
     load = [int(random.randint(int(Fcmin), int(Fcmax))), int(random.randint(int(Satmin), int(Satmax)))]
-    PAS = numeroS
-    PAD = numeroD
-    return {'FC': load[0], 'SAT': load[1], 'PAS': PAS, 'PAD': PAD}
+    loadB = [nS, nD]
+    return {'FC': load[0], 'SAT': load[1], 'PAS': loadB[0], 'PAD': loadB[1]}
 
-@app.route('/homepage')
-def home_page():
-    return render_template("homepage.html")
 
-@app.route('/fetch_dataPNI', methods=['POST','GET'])
-def FetchDataPNI():
+@app.route('/fetch_button_pni', methods=['POST', 'GET'])
+def fetch_button_pni():
+    global PNI
+    if "PNI" in request.form:
+        PNI = 1
+    else:
+        PNI = 0
+    return render_template('index.html')
+
+
+@app.route('/fetch_dataPNI', methods=['POST', 'GET'])
+def fetch_data_pni():
     global numeroS
     global numeroD
     if request.method == "POST":
@@ -70,8 +78,9 @@ def FetchDataPNI():
         numeroD = request.form['numberD']
         return render_template('control.html')
 
-@app.route('/fetch_dataFC', methods=['POST','GET'])
-def FetchDataFC():
+
+@app.route('/fetch_dataFC', methods=['POST', 'GET'])
+def fetch_data_fc():
     global Fcmin
     global Fcmax
     if request.method == "POST":
@@ -79,8 +88,9 @@ def FetchDataFC():
         Fcmax = request.form['FCmax']
         return render_template('control.html')
 
-@app.route('/fetch_dataSat', methods=['POST','GET'])
-def FetchDataSat():
+
+@app.route('/fetch_dataSat', methods=['POST', 'GET'])
+def fetch_data_sat():
     global Satmin
     global Satmax
     if request.method == "POST":
@@ -88,9 +98,11 @@ def FetchDataSat():
         Satmax = request.form['Satmax']
         return render_template('control.html')
 
-@app.route('/control' , methods=['POST','GET'])
+
+@app.route('/control', methods=['POST', 'GET'])
 def control():
     return render_template("control.html")
 
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
